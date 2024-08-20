cd97ae28adfd9cc3a01db19a384d122c6f7a9d54
Eomaxl <souravsnigdh@gmail.com>
2024-08-21 00:41:55 +0530

    [BookStore-Backend] Added test class for adding a new author

--- src/main/kotlin/com/microservice/bookstore/controller/AuthorsController.kt
+++ src/main/kotlin/com/microservice/bookstore/controller/AuthorsController.kt
@@ -4,6 +4,8 @@ import com.microservice.bookstore.domain.dto.AuthorDto
 import com.microservice.bookstore.services.AuthorService
 import com.microservice.bookstore.toAuthorDto
 import com.microservice.bookstore.toAuthorEntity
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController
 class AuthorsController(private val authorService: AuthorService) {
 
     @PostMapping(path = ["/v1/authors"])
-    fun createAuthor(@RequestBody authorDto: AuthorDto): AuthorDto{
-        return authorService.save(authorDto.toAuthorEntity()).toAuthorDto()
+    fun createAuthor(@RequestBody authorDto: AuthorDto): ResponseEntity<AuthorDto> {
+        val createdAuthor = authorService.save(authorDto.toAuthorEntity()).toAuthorDto()
+        return ResponseEntity<AuthorDto>(createdAuthor, HttpStatus.CREATED)
     }
 }
